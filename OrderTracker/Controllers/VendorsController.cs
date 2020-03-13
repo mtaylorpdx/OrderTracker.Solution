1850f5dabfc38c537c199680dd6b0fc3079070b3
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-13 13:14:46 -0700

    add functionality to add and display orders by vendor

--- OrderTracker/Controllers/VendorsController.cs
+++ OrderTracker/Controllers/VendorsController.cs
@@ -26,5 +26,29 @@ namespace OrderTracker.Controllers
     {
       return View();
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string date, int breadCount, int pastryCount, int total)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(date, breadCount, pastryCount, total);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
