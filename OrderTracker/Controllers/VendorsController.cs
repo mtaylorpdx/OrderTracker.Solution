a73f25cf8dec680790be2a3c30c32458d8b89f9c
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-13 11:00:22 -0700

    add routes to create and view Vendors

--- OrderTracker/Controllers/VendorsController.cs
+++ OrderTracker/Controllers/VendorsController.cs
@@ -7,6 +7,24 @@ namespace OrderTracker.Controllers
 {
   public class VendorsController : Controller
   {
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
 
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> vendorList = Vendor.GetAll();
+      return View(vendorList);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
   }
 }
