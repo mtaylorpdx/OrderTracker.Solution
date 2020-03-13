7d7d3331e9eb9eab0df45319835c9d6dc1b21c58
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-13 10:33:52 -0700

    add Vendor class constructor

--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -4,6 +4,17 @@ namespace OrderTracker.Models
 {
   public class Vendor
   {
-    
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string VendorName {get;set;}
+    public int Id {get;}
+    public List<Order> Orders {get;set;}
+
+    public Vendor(string vendorName)
+    {
+      VendorName = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> {};
+    }
   }
 }
