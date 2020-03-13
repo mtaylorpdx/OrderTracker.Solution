6176079cc505afa6025cad705309e2f3493a0d98
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-13 10:29:14 -0700

    add constructor for Order class

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -4,6 +4,19 @@ namespace OrderTracker.Models
 {
   public class Order
   {
+    public string Date {get;set;}
+    public int breadCount {get;set;}
+    public int pastryCount {get;set;}
+    public int Total {get;set;}
 
+    public Order (string date, int breadCount, int pastryCount, int total)
+    {
+      Date = date;
+      BreadCount = breadCount;
+      pastryCount = pastryCount;
+      Total = total;
+      _instances.Add(this);
+      IDictionary = _instances.Count;
+    }
   }
 }
