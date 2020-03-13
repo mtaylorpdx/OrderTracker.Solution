6544538c4646a718ec83dc9f35811911977c1dd0
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-13 12:08:09 -0700

    add routes to New Order and Delete all

--- OrderTracker/Controllers/OrdersController.cs
+++ OrderTracker/Controllers/OrdersController.cs
@@ -7,5 +7,19 @@ namespace OrderTracker.Controllers
   public class OrdersController : Controller
   {
 
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+       Vendor vendor = Vendor.Find(vendorId);
+       return View(vendor);
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+
   }
 }
